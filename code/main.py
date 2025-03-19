ec3989c7dd94898da1d7e1dea3d5a86c259698f2
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-03-19 18:08:27 -0300

    add ICMP layer in packet builder

--- code/main.py
+++ code/main.py
@@ -8,7 +8,7 @@ import sys
 from arg_parser import Argument_Manager as ArgParser
 from port_scan  import Port_Scanner
 from bgrab      import Banner_Grabbing
-#from netmap     import Network_Mapper
+from netmap     import Network_Mapper
 from display    import *
 
 
@@ -20,23 +20,22 @@ class Main:
         self._commands_dict  = {
             'pscan':  Port_Scanner,
             'banner': Banner_Grabbing,
-            #'netmap': Network_Mapper
+            'netmap': Network_Mapper
         }
 
 
     def _handle_user(self) -> None:
         try:   self._validate_input()
         except KeyboardInterrupt:  sys.exit()
+        except PermissionError:    print(yellow("Run this command as root (sudo)."))
+        except IndexError:         print(yellow("Missing command name"))
         except Exception as error: print(unexpected_error(error))
 
     
     def _validate_input(self) -> None:
-        try: 
-            self._command   = sys.argv[1]
-            self._arguments = sys.argv[2:] if len(sys.argv) > 2 else list()
-            self._verify_if_the_command_exists()
-        except IndexError:
-            print(f'{yellow("Missing command name")}')
+        self._command   = sys.argv[1]
+        self._arguments = sys.argv[2:] if len(sys.argv) > 2 else list()
+        self._verify_if_the_command_exists()
 
 
     def _verify_if_the_command_exists(self) -> None:
@@ -51,23 +50,21 @@ class Main:
 
 
     def _run_command(self, arg_parser:ArgParser) -> None:
-        try:
-            strategy_class = self._commands_dict.get(self._command)
-            with strategy_class(arg_parser) as strategy:
-                strategy._execute()
-        except Exception as error:
-            print(f'{red("Error while trying to execute the command")}.\nERROR: {error}')
+        strategy_class = self._commands_dict.get(self._command)
+        with strategy_class(arg_parser) as strategy:
+            strategy._execute()
 
 
     @staticmethod
     def _display_description() -> None:
-        print('Repository: https://github.com/olivercalazans/DataSeeker\n'
-              'DataSeeker CLI is a tool for network exploration\n'
+        print('Repository: https://github.com/olivercalazans/netexplorer\n'
+              'NetXplorer CLI is a tool for network exploration\n'
               'Available commands:\n'
               f'{green("pscan")}....: Portscaning\n'
               f'{green("banner")}...: Banner Grabbing\n'
               f'{green("netmap")}...: Network Mapping\n'
               )
+
 
 
 if __name__ == '__main__':
