3c5edabfe1ed9139e5a1b5d833acd7c5fd48b9c0
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-03-20 17:56:58 -0300

    dissector class removed

--- code/pkt_dissector.py
+++ code/pkt_dissector.py
@@ -8,73 +8,30 @@ import struct, socket
 from type_hints import Raw_Packet
 
 
-class Dissector:
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        return False
-
-
-    FLAG_MAP = {
-            0b00010010: 'SYN-ACK', # (0b00000010 + 0b00010000)
-            0b00000010: 'SYN',
-            0b00010100: 'RST-ACK', # RST-ACK (0b00000100 + 0b00010000)
-            0b00000100: 'RST',
-            0b00000001: 'FIN'
-        }
+TCP_FLAG_MAP = {
+        0b00010010: 'SYN-ACK', # (0b00000010 + 0b00010000)
+        0b00000010: 'SYN',
+        0b00010100: 'RST-ACK', # RST-ACK (0b00000100 + 0b00010000)
+        0b00000100: 'RST',
+        0b00000001: 'FIN'
+    }
 
 
-    __slots__ = ('_packet', '_ip_header', '_tcp_header', '_source_ip', '_source_port', '_flags')
-
-    def __init__(self):
-        self._packet:Raw_Packet = None
-        self._ip_header:bytes   = None
-        self._tcp_header:bytes  = None
-        self._source_ip:str     = None
-        self._source_port:int   = None
-        self._flags:str|None    = None
-
-
-    def _dissect(self, packets:list[Raw_Packet]) -> list[dict|None]:
-        return [self._dissect_tcp_ip_packet(pkt) for pkt in packets]
+def dissect_tcp_packet(packet:Raw_Packet) -> dict|None:
+    try:
+        packet:Raw_Packet            = memoryview(packet)
+        ip_header:tuple[int, bytes]  = struct.unpack('!BBHHHBBH4s4s', packet[14:34])
+        tcp_header:tuple[int, bytes] = get_tcp_header(packet, ip_header)
+        source_ip:str                = socket.inet_ntoa(ip_header[8])
+        source_port:int              = tcp_header[0]
+        tcp_flags:str                = TCP_FLAG_MAP.get(tcp_header[5] & (0b00111111), 'Filtered')
+        return {'ip': source_ip, 'port': source_port, 'flags': tcp_flags}
+    except (IndexError, struct.error, ValueError):
+        return None
 
 
-    def _dissect_tcp_ip_packet(self, packet:Raw_Packet) -> dict|None:
-            try:
-                self._packet = memoryview(packet)
-                self._get_ip_header()
-                self._get_tcp_header()
-                self._get_source_ip()
-                self._get_source_port()
-                self._get_tcp_flags()
-                return {'ip': self._source_ip, 'port': self._source_port, 'flags': self._flags}
-            except (IndexError, struct.error, ValueError):
-                return None
-
-
-    def _get_ip_header(self) -> bytes:
-        ip_header       = self._packet[14:34]
-        ip_header       = struct.unpack('!BBHHHBBH4s4s', ip_header)
-        self._ip_header = ip_header
-
-
-    def _get_tcp_header(self) -> bytes:
-        ihl              = (self._ip_header[0] & 0x0F) * 4
-        tcp_header_start = 14 + ihl
-        tcp_header       = self._packet[tcp_header_start:tcp_header_start + 20]
-        self._tcp_header = struct.unpack('!HHLLBBHHH', tcp_header)
-
-
-    def _get_source_ip(self) -> None:
-        self._source_ip = socket.inet_ntoa(self._ip_header[8])
-
-
-    def _get_source_port(self) -> None:
-        self._source_port = self._tcp_header[0]
-
-
-    def _get_tcp_flags(self) -> None:
-        flags       = self._tcp_header[5]
-        self._flags = self.FLAG_MAP.get(flags & (0b00111111), 'Filtered')
+def get_tcp_header(packet:Raw_Packet, ip_header:tuple[int, bytes]) -> tuple[int, bytes]:
+    ihl              = (ip_header[0] & 0x0F) * 4
+    tcp_header_start = 14 + ihl
+    tcp_header       = packet[tcp_header_start:tcp_header_start + 20]
+    return  struct.unpack('!HHLLBBHHH', tcp_header)
