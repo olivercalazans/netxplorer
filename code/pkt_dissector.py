796bc9757cefce8d2bddc3e943c9594ede0e0fa6
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-03-20 22:35:35 -0300

    refactoring dissector

--- code/pkt_dissector.py
+++ code/pkt_dissector.py
@@ -4,7 +4,7 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software...
 
 
-import struct, socket
+import struct
 from type_hints import Raw_Packet
 
 
@@ -22,16 +22,16 @@ def dissect_tcp_packet(packet:Raw_Packet) -> dict|None:
         packet:Raw_Packet            = memoryview(packet)
         ip_header:tuple[int, bytes]  = struct.unpack('!BBHHHBBH4s4s', packet[14:34])
         tcp_header:tuple[int, bytes] = get_tcp_header(packet, ip_header)
-        source_ip:str                = socket.inet_ntoa(ip_header[8])
         source_port:int              = tcp_header[0]
         tcp_flags:str                = TCP_FLAG_MAP.get(tcp_header[5] & (0b00111111), 'Filtered')
-        return {'ip': source_ip, 'port': source_port, 'flags': tcp_flags}
+        
+        return {'port': source_port, 'flags': tcp_flags}
     except (IndexError, struct.error, ValueError):
         return None
-
+    
 
 def get_tcp_header(packet:Raw_Packet, ip_header:tuple[int, bytes]) -> tuple[int, bytes]:
-    ihl              = (ip_header[0] & 0x0F) * 4
-    tcp_header_start = 14 + ihl
-    tcp_header       = packet[tcp_header_start:tcp_header_start + 20]
-    return  struct.unpack('!HHLLBBHHH', tcp_header)
+    ihl:int          = (ip_header[0] & 0x0F) * 4
+    header_start:int = 14 + ihl
+    header_end:int   = header_start + 20
+    return struct.unpack('!HHLLBBHHH', packet[header_start:header_end])
