cfa96280c2530e895543ef907c9635df4d8ef58b
Oliver Calazans <oliver.calazans@gmail.com>
2025-03-22 13:35:34 -0300

    arg_parser finished. Class removed

--- code/arg_parser.py
+++ code/arg_parser.py
@@ -8,17 +8,14 @@ import argparse
 
 
 # PORTSCANNER ============================================================================================
-def pscan_definitions(parser:argparse.ArgumentParser) -> None:
+def pscan_definitions(parser:argparse.ArgumentParser, arguments:list) -> dict:
     parser.add_argument('host', type=str, help='Target IP/Hostname')
     parser.add_argument('-s', '--show', action='store_true', help='Display all statuses, both open and closed')
     parser.add_argument('-r', '--random', action='store_true', help='Use the ports in random order')
     parser.add_argument('-p', '--port', type=str, help='Specify a port to scan')
     parser.add_argument('-a', '--all', action='store_true', help='Scan all ports')
     parser.add_argument('-d', '--delay', nargs='?', const=True, default=False, help='Add a delay between packet transmissions')
-
-
-
-def get_portscan_arguments(parser:argparse.ArgumentParser) -> dict:
+    parser = parser.parse_args(arguments)
     return {
         'host':   parser.host,
         'show':   parser.show,
@@ -31,14 +28,12 @@ def get_portscan_arguments(parser:argparse.ArgumentParser) -> dict:
 
 
 # BANNER GRABBER ========================================================================================
-def bgrab_definitions(parser:argparse.ArgumentParser) -> None:
+def bgrab_definitions(parser:argparse.ArgumentParser, arguments:list) -> dict:
     PROTOCOLS = ['ftp', 'ssh', 'http', 'https']
     parser.add_argument('host', type=str, help='Target IP/Hostname')
     parser.add_argument('protocol', type=str, choices=PROTOCOLS, help='Protocol')
     parser.add_argument('-p', '--port', type=str, help='Specify a port to grab the banners')
-
-
-def get_bgrab_arguments(parser:argparse.ArgumentParser) -> dict:
+    parser = parser.parse_args(arguments)
     return {
         'host':     parser.host,
         'protocol': parser.protocol,
@@ -47,11 +42,9 @@ def get_bgrab_arguments(parser:argparse.ArgumentParser) -> dict:
 
 
 # NETWORK MAPPER ========================================================================================
-def netmap_definitions(parser:argparse.ArgumentParser) -> None:
+def netmap_definitions(parser:argparse.ArgumentParser, arguments:list) -> dict:
     parser.add_argument('-p', '--port', action='store_true', help='Use ping instead of an ARP packet')
-
-
-def get_netmap_arguments(parser:argparse.ArgumentParser) -> dict:
+    parser = parser.parse_args(arguments)
     return{
         'port': parser.port
     }
@@ -61,16 +54,14 @@ def get_netmap_arguments(parser:argparse.ArgumentParser) -> dict:
 # ===============================================================================================================================
 
 DEFINITIONS = {
-    'pscan':  {'definitions': pscan_definitions,  'values': get_portscan_arguments},
-    'banner': {'definitions': bgrab_definitions,  'values': get_bgrab_arguments},
-    'netmap': {'definitions': netmap_definitions, 'values': get_netmap_arguments}
+    'pscan':  pscan_definitions,
+    'banner': bgrab_definitions,
+    'netmap': netmap_definitions,
 }
 
 
 # Method that will be called by the class
 def validate_and_get_flags(command:str, arguments:list) -> argparse.Namespace:
-    parser         = argparse.ArgumentParser(description='Argument Manager')
-    specific_class = DEFINITIONS.get(command)
-    specific_class['definitions'](parser)
-    parser.parse_args(arguments)
-    return specific_class['values'](parser)
+    parser       = argparse.ArgumentParser(description='Argument Manager')
+    command_args = DEFINITIONS.get(command)
+    return command_args(parser, arguments)
