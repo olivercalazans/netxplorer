f21bb4a1535591f3881ee62917383112683037b8
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2025-05-14 00:31:01 -0300

    add buffer in the sniffer socket

--- src/netxplorer/sniffing/sniffer.py
+++ src/netxplorer/sniffing/sniffer.py
@@ -8,10 +8,11 @@ import socket
 import ctypes
 import select
 from threading           import Thread
+from queue               import Queue
 from models.data         import Data
 from sniffing.bpf_filter import BPF_Filter
 from utils.network_info  import get_default_iface
-from utils.type_hints    import BPF_Instruction, BPF_Configured_Socket
+from utils.type_hints    import BPF_Instruction, BPF_Configured_Socket, Raw_Packet
 
 
 class Sniffer:
@@ -25,14 +26,16 @@ class Sniffer:
 
     
 
-    __slots__ = ('_data', '_protocols', '_running', '_sniffer', '_thread')
+    __slots__ = ('_data', '_protocols', '_running', '_sniffer', '_thread_sniffer', '_thread_store', '_queue')
 
     def __init__(self, data:Data, protocols:str) -> None:
         self._data:Data                     = data
         self._protocols:list                = protocols
         self._running:bool                  = True
         self._sniffer:BPF_Configured_Socket = None
-        self._thread:Thread                 = None
+        self._thread_sniffer:Thread         = None
+        self._thread_store:Thread           = None
+        self._queue:Queue                   = Queue()
 
     
 
@@ -49,8 +52,10 @@ class Sniffer:
 
 
     def _start_sniffing(self) -> None:
-        self._thread = Thread(target=self._sniff)
-        self._thread.start()
+        self._thread_store   = Thread(target=self._store_packets)
+        self._thread_sniffer = Thread(target=self._sniff)
+        self._thread_store.start()
+        self._thread_sniffer.start()
 
 
 
@@ -59,18 +64,31 @@ class Sniffer:
             readable, _, _= select.select([self._sniffer], [], [], 0.001)
             if readable:
                 packet, _ = self._sniffer.recvfrom(65535)
+                self._queue.put(packet)
+
+
+    
+    def _store_packets(self) -> None:
+        while self._running or not self._queue.empty():
+            try:
+                packet:Raw_Packet = self._queue.get(timeout=0.1)
                 self._data.raw_packets.append(packet)
+            except:
+                continue
 
 
 
     def stop_sniffing(self) -> list[dict]:
         self._running = False
-        self._thread.join()
+        self._thread_sniffer.join()
+        self._thread_store.join()
+        self._sniffer.close()
 
 
 
     def _create_sniffer(self) -> BPF_Configured_Socket:
         sniffer:socket.socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+        sniffer.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2**30)
         sniffer.bind((get_default_iface(), 0))
 
         bpf_filter:BPF_Instruction = BPF_Filter.get_filter(self._protocols)
@@ -97,7 +115,7 @@ class Sniffer:
 
 
 
-# Define a BPF filter structure
+# Defines a BPF filter structure
 class sock_filter(ctypes.Structure):
     _fields_ = [
         ("code", ctypes.c_ushort), #...: Operation Code
